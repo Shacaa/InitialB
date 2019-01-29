1a4592d3f2728646a34e3898154b016cc2c670cf
Shacaa <elshaca95@gmail.com>
2019-01-29 15:58:24 -0300

    update all

--- commands/update.js
+++ commands/update.js
@@ -4,18 +4,51 @@
  */
 
 const owner = '116758923603738625';
+const commands = [
+	"about",
+	"event",
+	"finish",
+	"help",
+	"invite",
+	"musicshare",
+	"reddit",
+	"report",
+	"roll",
+	"rps",
+	"spotify",
+	"testC",
+	"update",
+	"debug",
+	"ohoho"
+];
 
 exports.run = (client, reddit, spotify, message, args) => {
 
 	if(message.author.id !== owner){
 		return false;
 	}
+	updateFile(args[0], message);
+	
+};
+
+
+/*
+* Updates given command file. If "all" it updates every file.
+* recieves: file(string), message(class)
+* returns:
+*/
+function updateFile(file, message){
 	try{
-		delete require.cache[require.resolve(`./${args[0]}.js`)];
-		console.log(`command ${args[0]} updated`);
-		message.channel.send(`command ${args[0]} updated`);
+		if(file === 'all'){
+			for(let i = 0; i < commands.length; i++){
+				delete require.cache[require.resolve(`./${commands[i]}.js`)];
+			}
+		}else{
+			delete require.cache[require.resolve(`./${file}.js`)];
+		}
+		console.log(`${file} updated`);
+		message.channel.send(`${file} updated`);
 	}catch(err){
 		console.error(err);
 	}
-	
-};
+}
